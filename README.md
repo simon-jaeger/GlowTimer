e9189b7f2bcb765ef9f13b26f9bb181f399c42e0
Simon Jäger <simon-jaeger@users.noreply.github.com>
2018-12-25 12:31:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# GlowTimer
+
+A simple pomodoro timer.
+
+> The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.
+> The technique uses a timer to break down work into intervals,
+> traditionally 25 minutes in length, separated by short breaks.
+> These intervals are named pomodoros, the anglicized plural of the Italian word pomodoro (tomato),
+> after the tomato-shaped kitchen timer that Cirillo used as a university student.
+>
+> <cite>https://en.wikipedia.org/wiki/Pomodoro_Technique</cite>
+
+It's possible to set an alternative interval by passing a variable `pomodoro`
+through the query string:
+
+- [30min](https://glowtimer.netlify.com?pomodoro=30)
+- [45min](https://glowtimer.netlify.com?pomodoro=45)
+- [60min](https://glowtimer.netlify.com?pomodoro=60)
+
+---
+
+- Icons made by <a href="https://fontawesome.com" target="_blank">Font Awesome</a>
---
+++ app/script/main.js
@@ -0,0 +1,96 @@
+// utils
+////////////////////////////////////////////////////////////////////////////////
+const milisecsToMMSS = milisecs => {
+  const secs = Math.round(milisecs / 1000);
+  const mm = Math.floor(secs / 60);
+  const ss = secs % 60;
+  return `${mm >= 10 ? mm : "0" + mm}:${ss >= 10 ? ss : "0" + ss}`;
+};
+
+const $ = document.querySelector.bind(document);
+
+// main
+/////////////////////////// /////////////////////////////////////////////////////
+let pomodoro =
+  new URLSearchParams(window.location.search).get("pomodoro") || 25;
+
+let tick;
+let notification;
+let duration = pomodoro * 60 * 1000;
+let start = Date.now();
+let end = start + duration;
+let timeLeft = duration;
+
+const $mins = $(".mins");
+const $secs = $(".secs");
+const $ring = $(".progressRing_fill");
+const $play = $(".button.-play");
+const $pause = $(".button.-pause");
+
+function render() {
+  const mmss = milisecsToMMSS(timeLeft);
+  document.title = mmss + " – GlowTimer";
+  $mins.textContent = mmss.slice(0, 2);
+  $secs.textContent = mmss.slice(3, 5);
+  $ring.style.strokeDashoffset =
+    (1 - timeLeft / duration) * $ring.getAttribute("stroke-dasharray");
+}
+
+function playTimer() {
+  clearInterval(tick);
+  if (notification) notification.close();
+  $play.toggleAttribute("hidden");
+  $pause.toggleAttribute("hidden");
+  if (timeLeft < duration) {
+    console.log("timer resumed");
+    start = Date.now() - (duration - timeLeft);
+  } else {
+    console.log("timer started");
+    start = Date.now();
+  }
+  end = start + duration;
+  tick = setInterval(() => {
+    timeLeft = Math.round((end - Date.now()) / 1000) * 1000;
+    console.log(timeLeft);
+    render();
+    if (timeLeft <= 0) {
+      clearInterval(tick);
+      timeLeft = duration;
+      console.log("timer finished");
+      notify();
+    }
+  }, 1000);
+}
+
+function pauseTimer() {
+  clearInterval(tick);
+  $play.toggleAttribute("hidden");
+  $pause.toggleAttribute("hidden");
+  console.log("timer paused");
+}
+
+function resetTimer() {
+  clearInterval(tick);
+  if (notification) notification.close();
+  $play.removeAttribute("hidden");
+  $pause.setAttribute("hidden", "");
+  timeLeft = duration;
+  console.log("timer reset");
+  render();
+}
+
+function notify() {
+  notification = new Notification("GlowTimer", {
+    body: "Take a break!",
+    icon: "meta/icon-192x192.png",
+    requireInteraction: true,
+  });
+}
+
+// init
+////////////////////////////////////////////////////////////////////////////////
+Notification.requestPermission().then(permission => {
+  console.log("notification permission: " + permission);
+});
+
+render();
