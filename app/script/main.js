6a154f3df7ddcfeeb46ee68b4baa57b640df5974
Simon Jäger <simon-jaeger@users.noreply.github.com>
2018-12-27 11:38:19 +0100

    js fooling around

--- app/script/main.js
+++ app/script/main.js
@@ -1,28 +1,7 @@
-let timer;
-
-const state = {
-  pomodoro: 25,
-  // break: 5,
-  // phase: "pomodoro",
-  phaseEnd: 0,
-};
-
-function startTimer() {
-  state.phaseEnd = Date.now() + state.pomodoro * 60 * 1000;
-  timer = setInterval(() => {
-    const milisecsLeft = state.phaseEnd - Date.now();
-    const mmss = milisecsToMMSS(milisecsLeft);
-    document.title = mmss + " – Pomodoro";
-    $(".mins").textContent = mmss.slice(0, 2);
-    $(".secs").textContent = mmss.slice(3, 5);
-    if (milisecsLeft <= 0) {
-      clearInterval(timer);
-      window.alert("pomodoro over");
-    }
-  }, 1000);
-}
+// TODO: design clean algorithm
 
 // utils
+////////////////////////////////////////////////////////////////////////////////
 const milisecsToMMSS = milisecs => {
   const secs = Math.round(milisecs / 1000);
   const mm = Math.floor(secs / 60);
@@ -31,3 +10,37 @@ const milisecsToMMSS = milisecs => {
 };
 
 const $ = document.querySelector.bind(document);
+
+// main
+////////////////////////////////////////////////////////////////////////////////
+const state = {
+  timer: null,
+  pomodoro: 1 * 60 * 1000,
+  // break: 5,
+  // phase: "pomodoro",
+  phaseEnd: 0,
+};
+
+const $mins = $(".mins");
+const $secs = $(".secs");
+const $ring = $(".progressRing_fill");
+
+function startTimer() {
+  state.phaseEnd = Date.now() + state.pomodoro;
+  state.timer = setInterval(() => {
+    const milisecsLeft = state.phaseEnd - Date.now();
+    const mmss = milisecsToMMSS(milisecsLeft);
+    document.title = mmss + " – Pomodoro";
+    $mins.textContent = mmss.slice(0, 2);
+    $secs.textContent = mmss.slice(3, 5);
+    $ring.style.strokeDashoffset =
+      (1 - state.pomodoro / milisecsLeft) *
+      $ring.getAttribute("stroke-dasharray");
+    if (milisecsLeft <= 0) {
+      clearInterval(timer);
+      window.alert("pomodoro over");
+    }
+  }, 1000);
+}
+
+
